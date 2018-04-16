69407f706c4b4ff47b1c1e070c4a969605d8a793
LAPTOP-1TOLUL3G\dimin <diminemt@gmail.com>
2018-04-16 14:58:02 +0300

    Player Health Bar

---
+++ Assets/Camera & UI/PlayerHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class PlayerHealthBar : MonoBehaviour
+{
+
+    RawImage healthBarRawImage;
+    Player player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        healthBarRawImage = GetComponent<RawImage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {                  //negative so it moves in the right direction 
+        float xValue = -(player.healthAsPercentage / 2f) - 0.5f;
+        healthBarRawImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f); //changes the x position of the health bar image to give the illusion of movement 
+    }                                                              //the image is twice the displayed size, so we draw half of it
+}
